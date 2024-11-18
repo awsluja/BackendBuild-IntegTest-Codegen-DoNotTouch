03118264532d9394a38f017b6c3d1ba66dd6831a
Armando Luja <aluja@amazon.com>
2024-11-18 11:44:40 -0800

    chore: build locally

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "BackendBuildTest": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
